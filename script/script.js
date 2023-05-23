75a6bd801a6f49f9c206940011e7b30e62a99eed
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-05-23 14:19:24 +0200

    css-js done

--- script/script.js
+++ script/script.js
@@ -1,8 +1,21 @@
-function calcbmi() {
-    let weight = parseFloat(document.getElementById("weight").value)
-    let height = parseFloat(document.getElementById("height").value)
-    let bmi = weight / height**2
-    document.getElementById("answer").value = bmi.toFixed(2)
+function calcBmi() {
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    let bmi = (weight / height**2) * 10000;
 
+    document.getElementById('answer').value = bmi.toFixed(2)
+
+    if (bmi <=18.4) {
+        document.getElementById('answer').value = bmi.toFixed(2) + ' Underweight';
+    }else if (bmi <=24.9 && bmi >=18.5 ) {
+        document.getElementById('answer').value = bmi.toFixed(2) +' Healthy';
+    }else if(bmi <=39.9 && bmi >=25.0) {
+        document.getElementById('answer').value = bmi.toFixed(2) +' Overweight'
+    } 
+        
+    }
     
-}
+   
+
+
+
